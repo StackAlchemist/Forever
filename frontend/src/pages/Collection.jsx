77a0d1f6a15f91f563573dbece46da63676f93e1
OlathedevGuy <jolamideprince@gmail.com>
2025-02-23 00:02:16 +0100

    additional home content and collection

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,24 @@
+import { useContext, useState } from "react"
+import { ShopContext } from "../Context/ShopContext"
+
 const Collection = () => {
+  const { products } = useContext(ShopContext)
+  const [showFilter, setShowFilter] = useState(false)
+
   return (
-    <div>Collection</div>
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* filter options */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
+        {/* category filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '':'hidden'} sm:block`}>
+          <p className="mb-3 text-sm font-medium ">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2"></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Collection
